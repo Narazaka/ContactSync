ea02e498c5966fffb2fa1cef47db7a6878e1a565
Narazaka <info@narazaka.net>
2025-05-17 20:24:15 +0900

    ga / og:image

--- docs~/astro.config.mjs
+++ docs~/astro.config.mjs
@@ -8,6 +8,28 @@ export default defineConfig({
 		starlight({
 			title: 'Contact Sync',
 			favicon: '/ContactSync.svg',
+			head: [
+				{
+					tag: "script",
+					attrs: {
+						async: true,
+						src: "https://www.googletagmanager.com/gtag/js?id=G-B8SY8B2N12",
+					}
+				},
+				{
+					tag: "script",
+					attrs: {
+						src: "/ga.js",
+					}
+				},
+				{
+					tag: "meta",
+					attrs: {
+						property: "og:image",
+						content: "https://contact-sync.vrchat.narazaka.net/ContactSync.svg",
+					}
+				}
+			],
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/Narazaka/ContactSync' }],
 			sidebar: [
 				{
@@ -15,6 +37,12 @@ export default defineConfig({
 					autogenerate: { directory: 'guides' },
 				},
 			],
+			locales: {
+				root: {
+					label: '日本語',
+					lang: 'ja',
+				}
+			}
 		}),
 	],
 });
